f352855472c7108707c2f0c0f64a6b9e54a379da
Amadeus <a.turner3x@gmail.com>
2023-06-20 23:46:19 -0400

    added user and thought models

---
+++ models/user.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  // Define user schema fields
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
