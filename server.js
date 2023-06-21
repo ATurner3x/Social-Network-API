54ad0224ccbc5bea30ba9125c97bd8e8a0729876
Amadeus <a.turner3x@gmail.com>
2023-06-20 23:35:31 -0400

    started basic server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+//import required packages
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+//middleware
+const app = express();
+app.use(express.json());
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
