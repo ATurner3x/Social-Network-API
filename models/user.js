e391761ff7b0e0c16bcbc89c31c288e09adac709
Amadeus <a.turner3x@gmail.com>
2023-06-23 23:12:09 -0400

    added user.js model

--- models/user.js
+++ models/user.js
@@ -1,8 +1,30 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  // Define user schema fields
-});
+const { Schema } = mongoose;
 
-const User = mongoose.model('User', userSchema);
-module.exports = User;
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    match: [/.+@.+\..+/, 'Please enter a valid email address'],
+  },
+  thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Thought',
+    },
+  ],
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+});
