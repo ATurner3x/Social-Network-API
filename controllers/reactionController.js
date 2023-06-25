5d263dab93051dd322c3e66b7ada626f32b2cf49
Amadeus <a.turner3x@gmail.com>
2023-06-25 06:35:19 -0400

    added delte reaction funtion

--- controllers/reactionController.js
+++ controllers/reactionController.js
@@ -1,6 +1,8 @@
 const Thought = require('../models/thought');
 
 // Controller functions
+
+// Create thought
 const createReaction = async (req, res) => {
   try {
     const { thoughtId } = req.params;
@@ -22,4 +24,31 @@ const createReaction = async (req, res) => {
     res.status(500).json(err);
   }
 };
+
+const deleteReaction = async (req, res) => {
+    try {
+      const { thoughtId, reactionId } = req.params;
+  
+      const updatedThought = await Thought.findByIdAndUpdate(
+        thoughtId,
+        { $pull: { reactions: { _id: reactionId } } },
+        { new: true }
+      );
+  
+      if (!updatedThought) {
+        res.status(404).json({ message: 'Thought or reaction not found' });
+        return;
+      }
+  
+      res.json(updatedThought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  };
+  
+  module.exports = {
+    createReaction,
+    deleteReaction,
+  };
+
 
