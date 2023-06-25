10c1c5ca0ca98b733c0fd4357a492c786c753eb7
Amadeus <a.turner3x@gmail.com>
2023-06-25 06:27:53 -0400

    worked on wrong file

---
+++ controllers/thoughtController.js
@@ -0,0 +1,16 @@
+const Thought = require('../models/thought');
+const User = require('../models/user');
+
+// Controller functions
+
+// Create thought
+
+const getAllThoughts = async (req, res) => {
+  try {
+    const thoughts = await Thought.find().populate('reactions');
+    res.json(thoughts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
