986f826b13ded74eeddc147fcf4fdb4ef1af1529
Amadeus <a.turner3x@gmail.com>
2023-06-25 06:18:57 -0400

    remove a friend funtion completed

--- controllers/userController.js
+++ controllers/userController.js
@@ -106,3 +106,40 @@ const addFriend = async (req, res) => {
     res.status(500).json(err);
   }
 };
+
+const removeFriend = async (req, res) => {
+  try {
+    const { userId, friendId } = req.params;
+
+    const user = await User.findByIdAndUpdate(
+      userId,
+      { $pull: { friends: friendId } },
+      { new: true }
+    );
+
+    const friend = await User.findByIdAndUpdate(
+      friendId,
+      { $pull: { friends: userId } },
+      { new: true }
+    );
+
+    if (!user || !friend) {
+      res.status(404).json({ message: 'User or friend not found' });
+      return;
+    }
+
+    res.json({ user, friend });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+module.exports = {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+};
