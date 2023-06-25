20b46d7518278db2a78d22ce4eafd63cd6727f40
Amadeus <a.turner3x@gmail.com>
2023-06-25 06:14:15 -0400

    create user / update user

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,6 +14,8 @@ const getAllUsers = async (req, res) => {
   }
 };
 
+// Get user by ID
+
 const getUserById = async (req, res) => {
     try {
       const user = await User.findById(req.params.userId).populate('thoughts friends');
@@ -21,6 +23,33 @@ const getUserById = async (req, res) => {
         res.status(404).json({ message: 'User not found' });
         return;
       }
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  };
+
+  // Create user
+  const createUser = async (req, res) => {
+    try {
+      const { username, email } = req.body;
+  
+      const user = await User.create({ username, email });
+  
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  };
+
+  // Update user
+
+  const updateUser = async (req, res) => {
+    try {
+      const user = await User.findByIdAndUpdate(req.params.userId, req.body, {
+        new: true,
+        runValidators: true,
+      });
       res.json(user);
     } catch (err) {
       res.status(500).json(err);
